c06ed2696c26d46228e427aaa58d7f780852a74b
r-spiel <richelle.mehlhaff@gmail.com>
2020-12-27 13:04:33 -0800

    wave 2 Timeline component

--- src/App.js
+++ src/App.js
@@ -3,30 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
-import TimelineEvent from './components/TimelineEvent';
 
 function App() {
-  console.log(timelineData);
-
-  const event = {
-    'person': 'Adele Goldberg',
-    'status': 'In Smalltalk, everything happens somewhere else.',
-    'timeStamp': '2018-05-18T22:12:03Z'
-  }
   // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{timelineData.person}'s social media feed</h1>
       </header>
       <main className="App-main">
-        <p>Timeline Event</p>
-        < TimelineEvent 
-          person={event.person} 
-          status={event.status}
-          timeStamp={event.timeStamp}
+        < Timeline 
+          events={timelineData.events}
         />
-
       </main>
     </div>
   );
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
   
-  return;
+  const events = props.events.map((event, i) => {
+    return (
+      <div key={i}>
+        <TimelineEvent
+          person={event.person}
+          status={event.status}
+          timeStamp={event.timeStamp}
+        />
+      </div>
+    )
+  })
+
+  return (
+      <div className="timeline">{events}</div>
+      // I update the timeline CSS to be 80% as 30% looked too narrow 
+  );
 }
 
 export default Timeline;
