1395bf6aebfad64dac8ad149afab9a372427ec50
r-spiel <richelle.mehlhaff@gmail.com>
2020-12-27 11:41:03 -0800

    wave 1: TimelineEvent component

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+
+
+const TimelineEvent = (props) => {
   
-  return;
+  return (
+    <div className="timeline-event">
+      <h3 className="event-person">{props.person}</h3>
+      <p className="event-status">{props.status}</p>
+      <p className="event-time">< Timestamp time={props.timeStamp} /></p>
+
+    </div>
+  );
 }
 
 export default TimelineEvent;
